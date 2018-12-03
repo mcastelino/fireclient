52fe32915fe43e1401646c17e15e56521aba5f90
Manohar Castelino <manohar.r.castelino@intel.com>
2018-12-03 12:24:12 -0800

    Simple test to create an instance with vsock enabled
    Simple test to create an instance with vsock enabled.
    
    To test vsock, within the VM with the CID 3
    ```
    socat - SOCKET-LISTEN:40:0:x00x00x00x04x00x00x03x00x00x00x00x00x00x00
    ```
    On the host
    ```
    socat - SOCKET-CONNECT:40:0:x00x00x00x04x00x00x03x00x00x00x00x00x00x00
    
    ```
    
    Now you can interact between the host and the guest
    
    Signed-off-by: Manohar Castelino <manohar.r.castelino@intel.com>

---
+++ instance.go
@@ -0,0 +1,194 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-openapi/strfmt"
+
+	httptransport "github.com/go-openapi/runtime/client"
+	"github.com/mcastelino/fireclient/client"
+	models "github.com/mcastelino/fireclient/client/models"
+	ops "github.com/mcastelino/fireclient/client/operations"
+)
+
+func NewFireClient(socketPath string) *client.Firecracker {
+	httpClient := client.NewHTTPClient(strfmt.NewFormats())
+
+	socketTransport := &http.Transport{
+		DialContext: func(ctx context.Context, network, path string) (net.Conn, error) {
+			addr, err := net.ResolveUnixAddr("unix", socketPath)
+			if err != nil {
+				return nil, err
+			}
+
+			return net.DialUnix("unix", nil, addr)
+		},
+	}
+
+	transport := httptransport.New(client.DefaultHost, client.DefaultBasePath, client.DefaultSchemes)
+	transport.Transport = socketTransport
+
+	httpClient.SetTransport(transport)
+
+	return httpClient
+}
+
+func getInstanceState(fireClient *client.Firecracker) {
+	{
+		resp, err := fireClient.Operations.DescribeInstance(nil)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+		fmt.Printf("%#v\n", resp.Payload)
+	}
+
+	{
+		resp, err := fireClient.Operations.GetMachineConfig(nil)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+		fmt.Printf("%#v\n", resp.Payload)
+	}
+
+	{
+		resp, err := fireClient.Operations.GetMmds(nil)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+		fmt.Printf("%#v\n", resp.Payload)
+	}
+}
+
+func main() {
+
+	fireClient := NewFireClient("/tmp/firecracker.sock")
+
+	if fireClient == nil {
+		os.Exit(1)
+	}
+
+	getInstanceState(fireClient)
+
+	/*
+		curl --unix-socket /tmp/firecracker.sock -i \
+		    -X PUT 'http://localhost/boot-source'   \
+		    -H 'Accept: application/json'           \
+		    -H 'Content-Type: application/json'     \
+		    -d '{
+		        "kernel_image_path": "./vmlinux",
+		        "boot_args": "console=ttyS0 reboot=k panic=1 pci=off"
+		    }'
+	*/
+	{
+		kernel := "./vmlinux"
+		cmdLine := "console=ttyS0 reboot=k panic=1 pci=off"
+		bootSrcParams := ops.NewPutGuestBootSourceParams()
+		src := &models.BootSource{
+			KernelImagePath: &kernel,
+			BootArgs:        cmdLine,
+		}
+
+		bootSrcParams.SetBody(src)
+
+		_, err := fireClient.Operations.PutGuestBootSource(bootSrcParams)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+	}
+
+	/*
+		curl --unix-socket /tmp/firecracker.sock -i \
+		    -X PUT 'http://localhost/drives/rootfs' \
+		    -H 'Accept: application/json'           \
+		    -H 'Content-Type: application/json'     \
+		    -d '{
+		        "drive_id": "rootfs",
+		        "path_on_host": "./hello-rootfs.ext4",
+		        "is_root_device": true,
+		        "is_read_only": false
+		    }'
+	*/
+	{
+		driveID := "rootfs"
+		hostPath := "./hello-rootfs.ext4"
+		driveParams := ops.NewPutGuestDriveByIDParams()
+		driveParams.SetDriveID(driveID)
+		isReadOnly := false
+		isRootDevice := true
+		drive := &models.Drive{
+			DriveID:      &driveID,
+			IsReadOnly:   &isReadOnly,
+			IsRootDevice: &isRootDevice,
+			PathOnHost:   &hostPath,
+		}
+		driveParams.SetBody(drive)
+		_, err := fireClient.Operations.PutGuestDriveByID(driveParams)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+	}
+
+	/*
+		curl --unix-socket /tmp/firecracker.sock -i \
+		     -X PUT "http://localhost/vsocks/root" \
+		     -H "accept: application/json" \
+		     -H "Content-Type: application/json" \
+		     -d "{
+		            \"id\": \"root\",
+		            \"guest_cid\": 3
+		         }"
+	*/
+	{
+		vsockParams := ops.NewPutGuestVsockByIDParams()
+		vsockID := "root"
+		vsock := &models.Vsock{
+			GuestCid: 3,
+			ID:       &vsockID,
+		}
+		vsockParams.SetID(vsockID)
+		vsockParams.SetBody(vsock)
+		_, _, err := fireClient.Operations.PutGuestVsockByID(vsockParams)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+	}
+
+	/*
+		curl --unix-socket /tmp/firecracker.sock -i \
+		    -X PUT 'http://localhost/actions'       \
+		    -H  'Accept: application/json'          \
+		    -H  'Content-Type: application/json'    \
+		    -d '{
+		        "action_type": "InstanceStart"
+		     }'
+	*/
+
+	/* This is where the VM is actually launched */
+	{
+		actionParams := ops.NewCreateSyncActionParams()
+		actionInfo := &models.InstanceActionInfo{
+			ActionType: "InstanceStart",
+			//Payload:    "",
+		}
+		actionParams.SetInfo(actionInfo)
+		_, err := fireClient.Operations.CreateSyncAction(actionParams)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+	}
+
+	time.Sleep(250 * time.Millisecond)
+	getInstanceState(fireClient)
+}
