c1f4ee714720679c00a0d6204d5b9bbe4e33a540
Manohar Castelino <manohar.r.castelino@intel.com>
2018-12-17 18:40:40 -0800

    Do not explicitly setup a root. Sent it in via the command line
    Do not explicitly setup a root. Sent it in via the command line.
    This will allow the use of partitioned root filesystems.
    
    Signed-off-by: Manohar Castelino <manohar.r.castelino@intel.com>

--- instance.go
+++ instance.go
@@ -89,7 +89,7 @@ func main() {
 	*/
 	{
 		kernel := "./vmlinux"
-		cmdLine := "console=ttyS0 reboot=k panic=1 pci=off"
+		cmdLine := "console=ttyS0 reboot=k panic=1 pci=off root=/dev/vda"
 		bootSrcParams := ops.NewPutGuestBootSourceParams()
 		src := &models.BootSource{
 			KernelImagePath: &kernel,
@@ -120,27 +120,6 @@ func main() {
 	{
 		driveID := "rootfs"
 		hostPath := "./hello-rootfs.ext4"
-		driveParams := ops.NewPutGuestDriveByIDParams()
-		driveParams.SetDriveID(driveID)
-		isReadOnly := false
-		isRootDevice := true
-		drive := &models.Drive{
-			DriveID:      &driveID,
-			IsReadOnly:   &isReadOnly,
-			IsRootDevice: &isRootDevice,
-			PathOnHost:   &hostPath,
-		}
-		driveParams.SetBody(drive)
-		_, err := fireClient.Operations.PutGuestDriveByID(driveParams)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(2)
-		}
-	}
-
-	{
-		driveID := "hotDummy"
-		hostPath := "./dummy.img"
 		driveParams := ops.NewPutGuestDriveByIDParams()
 		driveParams.SetDriveID(driveID)
 		isReadOnly := false
@@ -178,83 +157,11 @@ func main() {
 		}
 	}
 
-	/*
-		curl --unix-socket /tmp/firecracker.sock -i \
-		     -X PUT "http://localhost/vsocks/root" \
-		     -H "accept: application/json" \
-		     -H "Content-Type: application/json" \
-		     -d "{
-		            \"id\": \"root\",
-		            \"guest_cid\": 3
-		         }"
-	*/
-	{
-		vsockParams := ops.NewPutGuestVsockByIDParams()
-		vsockID := "root"
-		vsock := &models.Vsock{
-			GuestCid: 3,
-			ID:       &vsockID,
-		}
-		vsockParams.SetID(vsockID)
-		vsockParams.SetBody(vsock)
-		_, _, err := fireClient.Operations.PutGuestVsockByID(vsockParams)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(2)
-		}
-	}
-
-	/*
-		curl --unix-socket /tmp/firecracker.sock -i \
-		    -X PUT 'http://localhost/actions'       \
-		    -H  'Accept: application/json'          \
-		    -H  'Content-Type: application/json'    \
-		    -d '{
-		        "action_type": "InstanceStart"
-		     }'
-	*/
-
 	/* This is where the VM is actually launched */
 	{
 		actionParams := ops.NewCreateSyncActionParams()
 		actionInfo := &models.InstanceActionInfo{
 			ActionType: "InstanceStart",
-			//Payload:    "",
-		}
-		actionParams.SetInfo(actionInfo)
-		_, err := fireClient.Operations.CreateSyncAction(actionParams)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(2)
-		}
-	}
-
-	/* Make sure it really boots and sees the drive */
-	time.Sleep(5000 * time.Millisecond)
-	getInstanceState(fireClient)
-
-	/* Now update the driver to point to a new one and rescan */
-	{
-		driveID := "hotDummy"
-		hostPath := "./real.img"
-		driveParams := ops.NewPatchGuestDriveByIDParams()
-		driveParams.SetDriveID(driveID)
-		drive := &models.PartialDrive{
-			DriveID:    &driveID,
-			PathOnHost: &hostPath, //This is the only property that can be modified on the fly
-		}
-		driveParams.SetBody(drive)
-		_, err := fireClient.Operations.PatchGuestDriveByID(driveParams)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(2)
-		}
-	}
-
-	{
-		actionParams := ops.NewCreateSyncActionParams()
-		actionInfo := &models.InstanceActionInfo{
-			ActionType: "BlockDeviceRescan",
 			//Payload:    "",
 		}
 		actionParams.SetInfo(actionInfo)
